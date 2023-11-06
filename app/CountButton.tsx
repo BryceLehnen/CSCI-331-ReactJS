d7794d563414ca82459b8457203690f8b43d3328
unknown <dlkl46.bl@gmail.com>
2023-11-06 09:58:34 -0700

    full counter integration

--- app/CountButton.tsx
+++ app/CountButton.tsx
@@ -1,15 +1,24 @@
 import {useState, useEffect} from "react"
 
-export default function CountButton() {
+export default function CountButton({increment}) {
     let [count, setCount] = useState(0)
 
     function handleClick() {
-        setCount(count = count + 1)
+        
+        setCount(count = count + increment)
     }
+
+    function rollOverCount() {
+        if (count > 10) {
+            setCount(0)
+        }
+    }
+
+    useEffect(rollOverCount, [count])
 
     return (
         <div>
-            <button onClick={handleClick}>+1</button>
+            <button onClick={handleClick}>+{increment}</button>
             <div>{count}</div>
         </div>
     )
--- app/page.tsx
+++ app/page.tsx
@@ -2,5 +2,14 @@
 import CountButton from "./CountButton"
 
 export default function Page() {
-    return <CountButton />
+    return (
+    <div>
+        <CountButton increment={1} />
+        <CountButton increment={2} />
+        <hr />
+        <a href="https://github.com/BryceLehnen/CSCI-331-ReactJS">The Github Repo for this ReactJS project</a>
+        <hr />
+
+    </div>
+    )
 }
