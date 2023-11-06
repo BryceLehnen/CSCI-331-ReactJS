7c27dbb60c7dcd0183c59f8c7328c9f327d71166
unknown <dlkl46.bl@gmail.com>
2023-11-06 11:09:48 -0700

    start of displaying products

--- app/SearchBar.tsx
+++ app/SearchBar.tsx
@@ -1,16 +1,28 @@
 import {useState, useEffect} from "react"
 
-export default function SearchBar() {
+const SearchBar = (props) => {
     let [searchValue, setSearchValue] = useState("")
+    //const rawProductData = await fetch("https://fakestoreapi.com/products/1")
+    //const productData = rawProductData.json()
 
     function changeSearch(event) {
         setSearchValue(event.target.value)
     }
 
+    const filteredProducts = props.products.filter((product) => {
+        return product.includes(searchValue)
+    })
+
     return (
         <div>
             <input type="text" value={searchValue} onChange={changeSearch} placeholder="Search products"></input>
-            
+            <ul>
+                {filteredProducts.map((product) => {
+                    return <li key={product}>{product}</li>
+                })}
+            </ul>
         </div>
     )
 }
+
+export default SearchBar
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,25 @@
 "use client"
+import {useState, useEffect} from "react"
 import CountButton from "./CountButton"
 import SearchBar from "./SearchBar"
 
 export default function Page() {
+    const [productsState, setProductsState] = useState([])
+
+    useEffect(() => {
+        setTimeout(() => {
+            setProductsState([
+                "example1",
+                "example2",
+                "example3",
+                "example4",
+                "example5",
+            ])
+        }, 2000)
+    }, [])
+
+    const hasProducts = productsState.length > 0
+
     return (
     <div>
         <CountButton increment={1} />
@@ -10,7 +27,7 @@ export default function Page() {
         <hr />
         <a href="https://github.com/BryceLehnen/CSCI-331-ReactJS">The Github Repo for this ReactJS project</a>
         <hr />
-        <SearchBar />
+        {hasProducts ? <SearchBar products={productsState}/> : "Loading products..."}
     </div>
     )
 }
